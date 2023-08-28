823f2dd7fcb307c6ab6cb874e3f790458b5627db
Ashok <136793439+Ashok1676@users.noreply.github.com>
2023-08-28 14:45:16 +0530

    Update Math.java

--- Math.java
+++ Math.java
@@ -2,7 +2,7 @@ public class JavaMathExample1
 {    
     public static void main(String[] args)     
     {    
-        double x = 28;    
+        double x = 30;    
         double y = 4;    
           
         // return the maximum of two numbers  
